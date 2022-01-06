cd81d0f41dc25af3015a8af02516842c15ff5b8c
konstantin <konstantin.dudersky@gmail.com>
2022-01-06 22:21:18 +0300

    setup file

--- app/setup.sh
+++ app/setup.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# setup file for barcode_generator - https://github.com/Konstantin-Dudersky/barcode_generator
+# can be removed after installation
+
 echo
 echo "-----> Upgrading system:"
 sudo apt -y update
@@ -14,4 +19,26 @@ curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poet
 source ~/.poetry/env
 poetry config virtualenvs.in-project true
 
-chmod +x ../start.sh
+echo
+echo "-----> Download source code:"
+wget https://github.com/Konstantin-Dudersky/barcode_generator/tarball/master
+mkdir barcode_generator
+tar -xf master --strip-components 1 -C barcode_generator
+rm master
+
+echo
+echo "-----> Install poetry:"
+cd barcode_generator || exit
+poetry install
+cd ..
+
+echo
+echo "-----> Copy files:"
+cd barcode_generator || exit
+cp example.config.yaml config.yaml
+cp example.input.csv input.csv
+cd ..
+
+echo
+echo "-----> Make executable:"
+chmod +x barcode_generator/start.sh
